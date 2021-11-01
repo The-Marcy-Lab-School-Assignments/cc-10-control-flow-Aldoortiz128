1f812bf379b85efab96712adc16d1f463055e51c
aldoortiz128 <aldoortiz128@gmail.com>
2021-11-01 13:55:37 +0000

    Finished challenges

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 //1
-function greatestOfTwo(){
-  
+function greatestOfTwo(num1, num2){
+  if (num1 > num2) {return (num1)}
+  else if (num1 = num2) {return (num1)}
+  else {return (num2) }
 }
 
 //2
-function greatestOfThree(){
-  
+function greatestOfThree(num1, num2, num3){
+  if (num1 > num2 && num1 > num3) {return (num1)}
+ else if (num2 > num1 && num2 > num3) {return (num2)}
+ else {return (num3)}
 }
 
 //3
-function greatestOfFour(){
-  
+function greatestOfFour(num1, num2, num3, num4){
+  if (num1 > num2 && num1 > num3 && num1 > num4) {return (num1)}
+  else if (num2 > num1 && num2 > num3 && num2 > num4) {return (num2)}
+  else if (num3 > num1 && num3 > num2 && num3 > num4) {return (num3)}
+  else {return (num4)}
 }
 
 
 //4
-function leastOfFour(){
-  
+function leastOfFour(num1, num2, num3, num4){
+  if (num1 < num2 && num1 < num3 && num1 < num4) {return (num1)}
+  else if (num2 < num1 && num2 < num3 && num2 < num4) {return (num2)}
+  else if (num3 < num1 && num3 < num2 && num3 < num4) {return (num3)}
+  else {return (num4)}
 }
+
+console.log(greatestOfTwo(2, 4))
+console.log(greatestOfThree(3, 6, 9))
+console.log(greatestOfFour(2, 8, 9, 10))
+console.log(leastOfFour(1, 4, 5, 4))
